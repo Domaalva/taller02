8c054eeede2aa9818bf732a6f48740caa8a14a1d
Domaalva <139288690+Domaalva@users.noreply.github.com>
2026-06-24 19:34:35 +0000

    Taller02

--- taller02_short/scripts/main.js
+++ taller02_short/scripts/main.js
@@ -7,23 +7,44 @@
 const fmtUSD = new Intl.NumberFormat('es-EC', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 });
 
 let loadData = () => {
+    const tbody = document.getElementById('transacciones');
 
     try {
         fetch('https://raw.githubusercontent.com/DATA-DAWM/Datos/refs/heads/main/Coffee/Coffe_sales.xml')
             .then(response => response.text())
+
             .then(xml => {
                 const parser = new DOMParser();
                 const doc = parser.parseFromString(xml.trim(), 'application/xml');
                 return doc;
             })
             .then(doc => {
-                console.log(doc);                
+                tbody.innerHTML = '';
+                const rows = Array.from(doc.querySelectorAll('row')).slice(0, 20);
+
+                rows.forEach(row => {
+                    const date = row.querySelector('Date')?.textContent || '';
+                    const coffee = row.querySelector('coffee_name')?.textContent || '';
+                    const moneyValue = Number(row.querySelector('money')?.textContent || 0);
+                    const money = fmtUSD.format(moneyValue);
+
+                    const tr = document.createElement('tr');
+                    tr.innerHTML = `
+                        <td>${date}</td>
+                        <td>${coffee}</td>
+                        <td>${money}</td>
+                    `;
+
+                    tbody.appendChild(tr);
+                });
             })
+            .catch(err => {
+                console.error('Error cargando datos:', err);
+            });
 
     } catch (err) {
         console.error(err);
     }
-
 }
 
 window.addEventListener('DOMContentLoaded', loadData);
